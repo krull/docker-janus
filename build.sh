22b2ffc09cdcff37013c66f27c783923af5ecce4
Christophe Kamphaus <christophe.kamphaus@jemmic.com>
2020-03-02 18:00:08 +0100

    Enable IPv6 support for websockets

--- build.sh
+++ build.sh
@@ -70,7 +70,7 @@ if [ $JANUS_WITH_WEBSOCKETS = "1" ]; then
     mkdir ${BUILD_SRC}/libwebsockets/build
     cd ${BUILD_SRC}/libwebsockets/build
     # See https://github.com/meetecho/janus-gateway/issues/732 re: LWS_MAX_SMP
-    cmake -DLWS_MAX_SMP=1 -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_C_FLAGS="-fpic" ..
+    cmake -DLWS_MAX_SMP=1 -DLWS_IPV6=ON -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_C_FLAGS="-fpic" ..
     make
     make install
 fi
